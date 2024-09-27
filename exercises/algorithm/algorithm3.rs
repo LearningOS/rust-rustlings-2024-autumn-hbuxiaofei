7fd4a78e558fcda8b7144b95b0d855cfaa51d238
rayylee <rayylee@foxmail.com>
2024-09-27 14:38:09 +0800

    solution algorithm/algorithm3.rs
    Signed-off-by: rayylee <rayylee@foxmail.com>

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -5,8 +5,21 @@
 */
 // I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
+fn sort<T: Ord>(array: &mut [T]){
 	//TODO
+    let n = array.len();
+    for i in 0..n {
+        let mut swapped = false;
+        for j in 0..n - 1 - i {
+            if array[j] > array[j + 1] {
+                array.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
